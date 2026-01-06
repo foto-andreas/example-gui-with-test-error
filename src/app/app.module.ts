4594718d1682ddbd240e36739dd99800c8115e82
Andreas Schrell <andreas.schrell@set.de>
2026-01-06 13:45:40 +0100

    simplify project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,7 @@
-import { registerLocaleData } from "@angular/common";
-import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
-import localeDe from "@angular/common/locales/de";
-import localeEn from "@angular/common/locales/en";
 import { NgModule } from "@angular/core";
-import { MAT_TABS_CONFIG, MatTabsConfig } from "@angular/material/tabs";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-
-registerLocaleData(localeDe);
-registerLocaleData(localeEn);
 
 /**
  * Entry module for POSY-OfficeConnect.
@@ -23,14 +15,6 @@ registerLocaleData(localeEn);
         BrowserModule
     ],
     providers: [
-        {
-            provide: MAT_TABS_CONFIG,
-            useValue: {
-                animationDuration: "200ms",
-                preserveContent: true
-            } as MatTabsConfig
-        },
-        provideHttpClient(withInterceptorsFromDi())
     ]
 })
 export class AppModule {
